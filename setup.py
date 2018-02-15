f9a663a124814c43df6986a3d8f92c44b235aecf
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2018-02-15 19:11:33 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
   name='iscraper',
-  version='0.2',
+  version='0.3',
   description='Front end for get-iplayer to allow download by name.',
   url='https://github.com/oldironhorse/iscraper',
-  download_url='https://github.com/oldironhorse/iscraper/archive/0.2.tar.gz',
+  download_url='https://github.com/oldironhorse/iscraper/archive/0.3.tar.gz',
   author='Simon Redding',
   author_email='s1m0n.r3dd1ng@gmail.com',
   license='GPL 3.0',
